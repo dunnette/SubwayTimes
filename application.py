f9b48a41acea1144414be22ee4e6ce231151b8dc
Robert Dunnette <dunnette@gmail.com>
2016-12-30 23:23:45 -0500

    app to application

--- application.py
+++ application.py
@@ -3,27 +3,27 @@ import flask
 
 r = ST_Reader.Reader()
 
-app = flask.Flask(__name__)
+application = flask.Flask(__name__)
 
-@app.route('/routes', methods=['GET'])
+@application.route('/routes', methods=['GET'])
 def get_routes():
     return flask.jsonify({'data': r.get_routes(), 'update': r.get_last_update('trip_updates')})
 
-@app.route('/<route_id>/stops', methods=['GET'])
+@application.route('/<route_id>/stops', methods=['GET'])
 def get_stops(route_id):
     return flask.jsonify({'data': r.get_stops([route_id]), 'update': r.get_last_update('trip_updates')})
 
-@app.route('/<stop_id>/times', methods=['GET'])
+@application.route('/<stop_id>/times', methods=['GET'])
 def get_stop_times(stop_id):
 	return flask.jsonify({'data': r.get_stop_times([stop_id]), 'update': r.get_last_update('trip_updates')})
 
-@app.route('/<stop_id>/times/next', methods=['GET'])
+@application.route('/<stop_id>/times/next', methods=['GET'])
 def get_next_stop_time(stop_id):
 	return flask.jsonify({'data': r.get_next_stop_time(stop_id), 'update': r.get_last_update('trip_updates')})
 
-@app.route('/<stop_id>/name', methods=['GET'])
+@application.route('/<stop_id>/name', methods=['GET'])
 def get_stop_name(stop_id):
 	return flask.jsonify({'data': r.get_stop_name(stop_id), 'update': r.get_last_update('stops')})
 
 if __name__ == '__main__':
-    app.run()
+    application.run()
